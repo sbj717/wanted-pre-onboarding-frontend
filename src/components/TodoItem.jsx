9f2165d2037851bca911e9ed2092e3906b46c060
samuelseong <sbj717@gmail.com>
2023-04-09 17:53:44 +0900

    API 통신 외 기능 완성

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,14 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./TodoItem.scss";
 
-export default function TodoItem(todo) {
+export default function TodoItem({
+  todo,
+  handleCheckboxChange,
+  handleDeleteItem,
+  handleEditItem,
+}) {
+  const [editToggle, setEditToggle] = useState(false);
+  const [editText, setEditText] = useState("");
+
+  useEffect(() => {
+    setEditText(todo.text);
+  }, []);
+
+  const cancelEdit = () => {
+    setEditToggle(!editToggle);
+    setEditText(todo.text);
+  };
+
+  const completeEdit = () => {
+    handleEditItem(todo.id, editText);
+    setEditToggle(!editToggle);
+  };
+
   return (
-    <li>
+    <li className="itemWrap">
       <label>
-        <input type="checkbox" />
-        <span>{todo.text}</span>
+        <input
+          className="checkbox"
+          type="checkbox"
+          checked={todo.status}
+          onChange={() => handleCheckboxChange(todo.id)}
+        />
+        {editToggle === false ? (
+          <p>{todo.text}</p>
+        ) : (
+          <input
+            className="textbox"
+            type="text"
+            value={editText}
+            onChange={(e) => setEditText(e.target.value)}
+            data-testid="modify-input"
+          ></input>
+        )}
       </label>
-      <button data-testid="modify-button">수정</button>
-      <button data-testid="delete-button">삭제</button>
+      {editToggle === false ? (
+        <div className="button">
+          <button
+            onClick={() => setEditToggle(!editToggle)}
+            data-testid="modify-button"
+          >
+            수정
+          </button>
+          <button
+            onClick={() => handleDeleteItem(todo.id)}
+            data-testid="delete-button"
+          >
+            삭제
+          </button>
+        </div>
+      ) : (
+        <div className="button">
+          <button onClick={completeEdit} data-testid="submit-button">
+            제출
+          </button>
+          <button onClick={cancelEdit} data-testid="cancel-button">
+            취소
+          </button>
+        </div>
+      )}
     </li>
   );
 }
--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -3,21 +3,42 @@ import TodoItem from "../components/TodoItem";
 import "./Todo.scss";
 
 export default function Todo() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "사전과제 구현하기",
-      status: "true",
-    },
-    {
-      id: 2,
-      text: "사전과제 제출하기",
-      status: "false",
-    },
-  ]);
+  const [todos, setTodos] = useState([]);
   const [todoInput, setTodoInput] = useState("");
+  const currentId = todos.length > 0 ? todos[todos.length - 1].id : 0;
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todoInput.length > 0) {
+      setTodos([
+        ...todos,
+        { id: Number(currentId + 1), text: todoInput, status: false },
+      ]);
+      setTodoInput("");
+    }
+  };
+
+  const checkboxChange = (id) => {
+    let newTodos = [...todos];
+    newTodos.map((todo) =>
+      todo.id === id
+        ? (todo.status = todo.status === true ? false : true)
+        : todo
+    );
+    setTodos(newTodos);
+  };
+
+  const deleteItem = (id) => {
+    let newTodos = [...todos];
+    newTodos = newTodos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  const editItem = (id, text) => {
+    let newTodos = [...todos];
+    newTodos.map((todo) => (todo.id === id ? (todo.text = text) : todo));
+    setTodos(newTodos);
+  };
 
   return (
     <div className="todoContainer">
@@ -30,13 +51,20 @@ export default function Todo() {
             onChange={(e) => {
               setTodoInput(e.target.value);
             }}
+            data-testid="new-todo-input"
           ></input>
-          <button>추가</button>
+          <button data-testid="new-todo-add-button">추가</button>
         </form>
         <ul>
-          {todos.map((todo) => {
-            <TodoItem key={todo.id} todo={todo} />;
-          })}
+          {todos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              handleCheckboxChange={checkboxChange}
+              handleDeleteItem={deleteItem}
+              handleEditItem={editItem}
+            />
+          ))}
         </ul>
       </div>
     </div>
